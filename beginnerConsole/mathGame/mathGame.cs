58ae669abb49e63fc3f9a566ca92547054401f32
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-07-01 16:04:19 +0200

    Committing first draft of mathGame

--- beginnerConsole/mathGame/mathGame.cs
+++ beginnerConsole/mathGame/mathGame.cs
@@ -1,2 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Menu variables
+bool valid = false;
+string? readResult;
+string menuChoice = "";
+
+// Game variables
+Random random = new Random();
+int lives = 3;
+int answer = 0;
+int playerAnswer = 0;
+int int1;
+int int2;
+int score = 0;
+
+Console.WriteLine("Welcome to the math game");
+
+while (lives > 0) {
+    Console.WriteLine("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
+
+    do {
+        Console.WriteLine("Choose an option by typing its number");
+        readResult = Console.ReadLine();
+        if (String.IsNullOrEmpty(readResult) == false) {
+            if (readResult == "1" || readResult == "2" || readResult == "3" || readResult == "4" ) {
+                menuChoice = readResult;
+                valid = true;
+
+            }
+        }
+    } while(valid == false);
+
+    if (menuChoice == "4") {
+        int modResult;
+        do {
+            int1 = random.Next(1, 101);
+            int2 = random.Next(1, 101);
+            modResult = int1 % int2;
+        } while(modResult != 0);
+    }
+    else {
+        int1 = random.Next(1, 101);
+        int2 = random.Next(1, 101);
+    }
+
+    switch (menuChoice) {
+        case "1":
+            answer = int1 + int2;
+            Console.WriteLine($"What is {int1} plus {int2}?");
+            break;
+        case "2":
+            answer = int1 - int2;
+            Console.WriteLine($"What is {int1} minus {int2}?");
+            break;
+        case "3":
+            answer = int1 * int2;
+            Console.WriteLine($"What is {int1} times {int2}?");
+            break;
+        case "4":
+            answer = int1 / int2;
+            Console.WriteLine($"What is {int1} divided ny {int2}?");
+            break;
+    }
+
+    do {
+        readResult = Console.ReadLine();
+        if (readResult != null) {
+            valid = int.TryParse(readResult, out playerAnswer);
+            if (!valid) {
+                Console.WriteLine("Not a number");
+            }
+        }
+
+    } while(valid == false);
+
+    if (playerAnswer == answer) {
+        score += 1;
+        Console.WriteLine($"Correct answer!\nYour current score is {score}");
+    }
+    else {
+        lives -= 1;
+        Console.WriteLine($"Wrong answer\nYou have {lives} lives left");
+    }
+}
