e2ca437dcb184b06a748966bb5d864e03f54e51d
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-07-02 11:43:35 +0200

    Added code for writing to highScore.txt

--- beginnerConsole/mathGame/mathGame.cs
+++ beginnerConsole/mathGame/mathGame.cs
@@ -3,11 +3,6 @@ bool valid = false;
 string? readResult;
 string menuChoice = "";
 
-// Read from and write to file
-StreamReader reader = new StreamReader("highScores.txt");
-StreamWriter writer = new StreamWriter("highScores.txt");
-
-
 // Game variables
 Random random = new Random();
 int lives = 3;
@@ -26,14 +21,26 @@ while (lives > 0) {
         Console.WriteLine("Choose an option by typing its number");
         readResult = Console.ReadLine();
         if (String.IsNullOrEmpty(readResult) == false) {
-            if (readResult == "1" || readResult == "2" || readResult == "3" || readResult == "4" ) {
+            if (readResult == "1" || readResult == "2" || readResult == "3" || readResult == "4" || readResult == "5" ) {
                 menuChoice = readResult;
                 valid = true;
 
             }
         }
     } while(valid == false);
-
+    
+    if (menuChoice == "5") {
+        StreamReader reader = new StreamReader("highScores.txt");
+        string? line = reader.ReadLine();
+        while (line != null) {
+            Console.WriteLine($"Your current highscore is {line}!");
+            line = reader.ReadLine();
+        }
+        reader.Close();
+        Console.WriteLine("Press enter to return to the menu");
+        Console.ReadLine();
+    }
+    
     if (menuChoice == "4") {
         int modResult;
         do {
@@ -47,56 +54,48 @@ while (lives > 0) {
         int2 = random.Next(1, 101);
     }
 
-    switch (menuChoice) {
-        case "1":
-            answer = int1 + int2;
-            Console.WriteLine($"What is {int1} plus {int2}?");
-            break;
-        case "2":
-            answer = int1 - int2;
-            Console.WriteLine($"What is {int1} minus {int2}?");
-            break;
-        case "3":
-            answer = int1 * int2;
-            Console.WriteLine($"What is {int1} times {int2}?");
-            break;
-        case "4":
-            answer = int1 / int2;
-            Console.WriteLine($"What is {int1} divided ny {int2}?");
-            break;
-        case "5":
-            string? line = reader.ReadLine();
-            while (line != null) {
-                Console.WriteLine(line);
-                line = reader.ReadLine();
-            }
-            reader.Close();
-            break;
-
-
-    }
-
-    do {
-        readResult = Console.ReadLine();
-        if (readResult != null) {
-            valid = int.TryParse(readResult, out playerAnswer);
-            if (!valid) {
-                Console.WriteLine("Not a number");
-            }
+    if (menuChoice != "5") {
+    
+        switch (menuChoice) {
+            case "1":
+                answer = int1 + int2;
+                Console.WriteLine($"What is {int1} plus {int2}?");
+                break;
+            case "2":
+                answer = int1 - int2;
+                Console.WriteLine($"What is {int1} minus {int2}?");
+                break;
+            case "3":
+                answer = int1 * int2;
+                Console.WriteLine($"What is {int1} times {int2}?");
+                break;
+            case "4":
+                answer = int1 / int2;
+                Console.WriteLine($"What is {int1} divided ny {int2}?");
+                break;
         }
 
-    } while(valid == false);
+        do {
+            readResult = Console.ReadLine();
+            if (readResult != null) {
+                valid = int.TryParse(readResult, out playerAnswer);
+                if (!valid) {
+                    Console.WriteLine("Not a number");
+                }
+            }
+        } while(valid == false);
 
-    if (playerAnswer == answer) {
-        score += 1;
-        Console.WriteLine($"Correct answer!\nYour current score is {score}\n");
-    }
-    else {
-        lives -= 1;
-        Console.WriteLine($"Wrong answer\nYou have {lives} lives left\n");
+        if (playerAnswer == answer) {
+            score += 1;
+            Console.WriteLine($"Correct answer!\nYour current score is {score}\n");
+        }
+        else {
+            lives -= 1;
+            Console.WriteLine($"Wrong answer\nYou have {lives} lives left\n");
+        }   
     }
 }
-
-Console.WriteLine("You Lose!");
-
-// test
+StreamWriter writer = new StreamWriter("highScore.txt");
+writer.WriteLine(score);
+writer.Close();
+Console.WriteLine("You lose!");
