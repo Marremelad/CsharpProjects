3fde9e1afc952e074758d1b4c0774b1a6552bede
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-07-02 09:44:30 +0200

    Committing repository

--- beginnerConsole/mathGame/obj/Debug/net8.0/mathGame.AssemblyInfo.cs
+++ beginnerConsole/mathGame/obj/Debug/net8.0/mathGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mathGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a4ec263224a08e922e979b65fea65a9fa1bb8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e035d3061dbcc22c5a58483327eeb3665f31120")]
 [assembly: System.Reflection.AssemblyProductAttribute("mathGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mathGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
